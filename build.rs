990f8aa0319eda8bd724e195ce8f37893bdfa396
Mac <Mac@localhost>
2017-08-28 22:50:26 +0900

    Fix rust binding

--- build.rs
+++ build.rs
@@ -1,12 +1,18 @@
+extern crate gcc;
+// Generates the Rust header for the C API.
 extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
-    //println!("cargo:rustc-link-lib=bz2");
+    //gcc::compile_library("libBigWig.a", &["bwRead.c", "bwStats.c", "bwValues.c", "bwWrite.c", "io.c"]);
+    gcc::Build::new().file("bwRead.c").file("bwStats.c").file("bwValues.c").file("bwWrite.c").file("io.c").flag("-DNOCURL").compile("libBigWig.a");
+    // let here = env::
+    // println!("cargo:rustc-link-search=native={}",format!("{}", here.display()));
+    println!("cargo:rustc-link-lib=static=BigWig");
+    println!("cargo:rustc-link-lib=z");
+    //println!("cargo:rustc-link-lib=static=curl");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -15,6 +21,7 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
+        //.clang_arg(format!("-L{}", here.display()))
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
